34fc73cbb94de4149534e12371cf55262a4dddab
ashwajha <ashwanijha04@gmail.com>
2024-06-16 11:40:19 +0530

    Added Auth

--- routers/recipes.js
+++ routers/recipes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAll, getAllNames, getById, create, update, remove } = require("../controllers/recipes");
+const { 
+	getAll, 
+	getAllNames, 
+	getById, 
+	create, 
+	update, 
+	remove 
+} = require("../controllers/recipes");
 
 router.get("/", getAll);
 router.get("/names", getAllNames);  // New route for names
@@ -9,6 +16,9 @@ router.get("/:id", getById);
 router.post("/", create);
 router.put("/:id", update);
 router.delete("/:id", remove);
+
+// Route Chaining
+// router.route('/').get(getAllRecipes).post(create);
 
 
 module.exports = router;
